16cf506b48694c44f20bb3240b3f762fe0102d91
404402223@qq.com <404402223@qq.com>
2017-08-10 21:49:45 +0800

    add product list

--- my-project/src/pages/index.vue
+++ my-project/src/pages/index.vue
@@ -1,6 +1,24 @@
 <template>
-  <div class="hello">
-    index
+  <div class="index-wrap">
+    <div class="index-left">
+    	<div class="index-left-block">
+    		<h2>全部产品</h2>
+			
+			<template v-for="product in productList">
+				<h3>{{ product.title }}</h3>
+				<ul>
+					<li v-for="item in product.list">
+						<a :href="item.url">{{ item.name }}</a>
+						<span v-if="item.hot" class="hot-tag">HOT</span>
+					</li>
+				</ul>
+          		<div v-if="!product.last" class="hr"></div>
+			</template>
+    	</div>
+    	<div class="index-left-block">
+    		<h2>最新消息</h2>
+    	</div>
+    </div>
   </div>
 </template>
 
@@ -9,13 +27,120 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      productList: {
+        pc: {
+          title: 'PC产品',
+          list: [
+            {
+              name: '数据统计',
+              url: 'http://starcraft.com'
+            },
+            {
+              name: '数据预测',
+              url: 'http://warcraft.com'
+            },
+            {
+              name: '流量分析',
+              url: 'http://overwatch.com',
+              hot: true
+            },
+            {
+              name: '广告发布',
+              url: 'http://hearstone.com'
+            }
+          ]
+        },
+        app: {
+          title: '手机应用类',
+          last: true,
+          list: [
+            {
+              name: '91助手',
+              url: 'http://weixin.com'
+            },
+            {
+              name: '产品助手',
+              url: 'http://twitter.com',
+              hot: true
+            },
+            {
+              name: '智能地图',
+              url: 'http://maps.com'
+            },
+            {
+              name: '团队语音',
+              url: 'http://phone.com'
+            }
+          ]
+        }
+      }
     }
   }
 }
 </script>
 
 
-<style>
+<style scoped>
+.index-wrap {
+  width: 1200px;
+  margin: 0 auto;
+  overflow: hidden;
+}
+.index-left {
+  float: left;
+  width: 300px;
+  text-align: left;
+}
+.index-right {
+  float: left;
+  width: 900px;
+}
+.index-left-block {
+  margin: 15px;
+  background: #fff;
+  box-shadow: 0 0 1px #ddd;
+}
+.index-left-block .hr {
+  margin-bottom: 20px;
+}
+.index-left-block h2 {
+  background: #4fc08d;
+  color: #fff;
+  padding: 10px 15px;
+  margin-bottom: 20px;
+}
+.index-left-block h3 {
+  padding: 0 15px 5px 15px;
+  font-weight: bold;
+  color: #222;
+}
+.index-left-block ul {
+  padding: 10px 15px;
+}
+.index-left-block li {
+  padding: 5px;
+}
+.index-board-list {
+  overflow: hidden;
+}
+.index-board-item {
+  float: left;
+  width: 400px;
+  background: #fff;
+  box-shadow: 0 0 1px #ddd;
+  padding: 20px;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+.index-board-item-inner {
+  min-height: 125px;
+  padding-left: 120px;
+}
+
+.hot-tag {
+  background: red;
+  color: #fff;
+  margin-left: 20px;
+}
 
 </style>
