7d2a8224c0a7688ce661d3ae817928f400787acf
404402223@qq.com <404402223@qq.com>
2017-08-23 22:20:26 +0800

    add Dialog.vue

--- my-project/src/components/layout.vue
+++ my-project/src/components/layout.vue
@@ -5,11 +5,14 @@
         <img src="../assets/logo.png" class="head-logo">
         <div class="head-nav">
           <ul class="nav-list">
-            <li>登录</li>
+            <li> {{ username }}</li>
+            <li v-if="username!== ''" class="nav-pile">|</li>
+            <li v-if="username!== ''" @click="quit">退出</li>
+            <li v-if="username=== ''" @click="loginDialog">登录</li>
             <li class="nav-pile">|</li>
-            <li>注册</li>
-            <li class="nav-pile">|</li>
-            <li>关于</li>
+            <li v-if="username=== ''" @click="registerDialog">注册</li>
+            <li v-if="username=== ''" class="nav-pile">|</li>
+            <li @click="aboutDialog">关于</li>
           </ul>        
         </div>
       </div>
@@ -22,15 +25,52 @@
     <div class="app-foot">
       <p>© 2016 fishenal MIT</p>
     </div>
+    <my-dialog :is-show="isShowLoginDialog" @on-close="closeDialog('isShowLoginDialog')">
+      <login-form @has-log="onSuccessLog"></login-form>
+    </my-dialog>
+    <my-dialog :is-show="isShowRegisterDialog" @on-close="closeDialog('isShowRegisterDialog')">
+      <p>register</p>
+    </my-dialog>
+    <my-dialog :is-show="isShowAboutDialog" @on-close="closeDialog('isShowAboutDialog')">
+      <p>本报告在调研数据的基础上，采用定性与定量相结合的方式深入分析了专车市场发展的驱动因素与阻碍因素、专车市场背后的产业格局、专车企业的竞争格局、用户对专车市场的依赖程度、专车对其他交通工具运力的补充效应等，通过这五个章节的研究反映专车市场的发展态势和面临的问题。报告力求客观、深入、准确地反映中国专车市场发展情况，为政府、企事业单位和社会各界提供决策依据。 </p>
+    </my-dialog>
   </div>
 </template>
 
 <script>
+import Dialog from '../components/base/dialog'
+import loginForm from '../components/base/loginForm'
 export default {
+  components : {
+    MyDialog: Dialog,
+    loginForm,
+  },
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isShowAboutDialog: false,
+      isShowRegisterDialog: false,
+      isShowLoginDialog: false,     
+      username: ''
+    }
+  }, 
+  methods: {
+    aboutDialog() {
+      this.isShowAboutDialog = true
+    }, 
+    loginDialog() {
+      this.isShowLoginDialog = true
+    },
+    registerDialog() {
+      this.isShowRegisterDialog = true
+    },
+    closeDialog(attr) {
+      this[attr] = false
+    }, 
+    onSuccessLog (data) {
+      console.log(data)
+      this.closeDialog ('isShowLoginDialog')
+      this.username = data
     }
   }
 }
@@ -38,6 +78,11 @@ export default {
 
 
 <style>
+/* http://meyerweb.com/eric/tools/css/reset/ 
+   v2.0 | 20110126
+   License: none (public domain)
+*/
+
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -58,7 +103,6 @@ time, mark, audio, video {
   font: inherit;
   vertical-align: baseline;
 }
-
 /* HTML5 display-role reset for older browsers */
 article, aside, details, figcaption, figure, 
 footer, header, hgroup, menu, nav, section {
@@ -99,18 +143,17 @@ body {
   line-height: 90px;
   width: 100%;
 }
-
 .app-head-inner {
   width: 1200px;
   margin: 0 auto;
 }
-
 .head-logo {
   float: left;
+}
+.app-head-inner img {
   width: 50px;
   margin-top: 20px;
 }
-
 .head-nav {
   float: right;
 }
@@ -124,7 +167,6 @@ body {
 .nav-pile {
   padding: 0 10px;
 }
-
 .app-foot {
   text-align: center;
   height: 80px;
@@ -133,5 +175,53 @@ body {
   background: #e3e4e8;
   clear: both;
   margin-top: 30px;
+}
+.container {
+  width: 1200px;
+  margin: 0 auto;
+}
+.hr {
+  height: 1px;
+  width: 100%;
+  background: #ddd;
+}
+.button {
+  background: #4fc08d;
+  color: #fff;
+  display: inline-block;
+  padding: 10px 20px;
+  cursor: pointer;
+}
+.button:hover {
+  background: #4fc08d;
+}
+.g-form {
+
+}
+.g-form-line {
+  padding: 15px 0;
+}
+.g-form-label {
+  width: 100px;
+  font-size: 16px;
+  display: inline-block;
+}
+.g-form-input {
+  display: inline-block;
+}
+.g-form-input input {
+  height: 30px;
+  width: 200px;
+  line-height: 30px;
+  vertical-align: middle;
+  padding: 0 10px;
+  border: 1px solid #ccc;
+}
+.g-form-btn {
+  padding-left: 100px;
+}
+.g-form-error {
+  color: red;
+  padding-left: 15px;
 }
 </style>
